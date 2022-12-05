66c2dc7c8041cde7ffbd38716dc484a951e78b1e
felipe <felipemoreira1@gmail.com>
2022-12-05 20:00:04 -0300

    Dia 05 - Problema 02

--- day05/lib/day05.ex
+++ day05/lib/day05.ex
@@ -34,9 +34,6 @@ defmodule Day05 do
 
                     end)
 
-
-    # {parsed_crates, moves_list}
-
     # Pego a lista dos movimentos
     moves_list = String.split(moves, "\n", trim: true)
 
@@ -46,6 +43,59 @@ defmodule Day05 do
       [[quantity], [from], [to]] = Regex.scan(~r/\d+/, moveString)
       parsed_move = [String.to_integer(quantity), String.to_integer(from), String.to_integer(to)]
       apply_move(acc, parsed_move)
+    end)
+
+    # Pego o resultado e exibo
+    resulting_state
+    |> Enum.map(fn {_key, value} -> 
+      [head | _] = value
+      head |> String.trim |> String.replace(~r/\[|\]/, "")
+    end)
+    |> Enum.join("")
+
+  end
+
+  # ======= Problema 02 - Mover várias caixas em um só movimento
+  def updated_organize_stacks do
+
+    input = File.read!("./input.txt")
+
+    [number_of_crates] = Regex.run(~r/\b[\s\d]+\b/, input)
+    crate_labels = number_of_crates |> String.trim |> String.split
+
+    # Map que será usado para amazenar as listas de caixas
+    map_of_crates = Enum.reduce(crate_labels, %{}, fn label, acc ->
+      Map.put(acc, String.to_integer(label), [])
+    end)
+
+    # Separo o estado inicial da lista dos movimentos
+    [crates, moves] = String.split(input, number_of_crates, trim: true)
+
+    # Faço o 'parse' do estado inicial
+    parsed_crates = crates
+                    |> String.split("\n", trim: true)
+                    |> Enum.map(fn line ->
+                      String.graphemes(line)
+                      |> Enum.chunk_every(4)
+                      |> Enum.map(fn chunk -> Enum.join(chunk) end)
+                    end)
+                    |> Enum.reverse
+                    |> tl 
+                    |> Enum.reduce(map_of_crates, fn item, acc -> 
+                      fill_lists(acc, item) 
+
+                    end)
+
+    # Pego a lista dos movimentos
+    moves_list = String.split(moves, "\n", trim: true)
+
+
+    # Aplico o(s) movimento
+    resulting_state = Enum.reduce(moves_list, parsed_crates, fn moveString, acc -> 
+
+      [[quantity], [from], [to]] = Regex.scan(~r/\d+/, moveString)
+      parsed_move = [String.to_integer(quantity), String.to_integer(from), String.to_integer(to)]
+      apply_moves(acc, parsed_move)
     end)
 
     # Pego o resultado e exibo
@@ -94,4 +144,19 @@ defmodule Day05 do
 
   end
 
+  # - Aplica varios movimento de uma só vez
+  defp apply_moves(map_of_lists, move) do
+
+    [quantity, from, to] = move
+
+    from_list = Map.get(map_of_lists, from)
+    from_length = length(from_list)
+    from_head = Enum.take(from_list, quantity)
+    from_tail = Enum.take(from_list, quantity - from_length) 
+
+    to_list = from_head ++ Map.get(map_of_lists, to)
+
+    Map.put(map_of_lists, from, from_tail) |> Map.put(to, to_list)
+    
+  end
 end
