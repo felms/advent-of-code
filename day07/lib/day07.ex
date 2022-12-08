96f4a80a0c666e60e358c20deb111ba3fb6dd794
felipe <felipemoreira1@gmail.com>
2022-12-08 00:33:13 -0300

    Dia 07 - Problema 01

--- day07/lib/day07.ex
+++ day07/lib/day07.ex
@@ -6,21 +6,54 @@ defmodule Day07 do
   # ======= Problema 01 - Soma dos diretorios com menos de 100000
   def sum_directories do
 
-    #  !!!!!! ----- BUG -----
-    # Diretórios com o mesmo nome e caminhos (path) diferentes
+    # Limpando o input, fazendo o 'parse' dos comandos e gerando um mapa com os diretórios
+    input = File.read!("./input.txt")
+            |> String.split("\n",trim: true)
+            |> Enum.reduce({nil, %{}}, fn command, acc -> 
+              {path, map} = acc
 
-    # Limpando o input e gerando um mapa com os diretórios
-    tree = File.read!("./input.txt")
-    |> String.replace(~r/(\$\s)|(cd \.\.)|\r|ls/, "") # limpando o input
-    |> String.split("cd",trim: true)
-    |> Enum.map(fn item -> String.split(item, "\n", trim: true) end)
-    |> Enum.reduce(%{}, fn [dir | items], acc -> Map.put(acc, String.trim_leading(dir), items) end)
+              cond do
+                command === "$ cd /" -> 
+                  new_path = "/"
+                  internal_map = Map.put(%{}, "dirs", []) |> Map.put("files", 0)
+                  new_map = Map.put(map, new_path, internal_map)
+                  {new_path, new_map}
+                command === "$ cd .." ->
+                  new_path = String.split(path, "-") |> List.delete_at(-1) |> Enum.join("-")
+                  {new_path, map}
+                String.contains?(command, "$ cd ") ->
+                  dir_name = String.replace(command, "$ cd ", "")
+                  new_path = path <> "-" <> dir_name
+                  internal_map = Map.put(%{}, "dirs", []) |> Map.put("files", 0)
+                  new_map = Map.put(map, new_path, internal_map)
+                  {new_path, new_map}
+                command === "$ ls" -> acc
+                String.contains?(command, "dir ") ->
+                  [ _prefix , dir_name] = String.split(command)
+                  dir_path = path <> "-" <> dir_name
+                  curr_internal_map = Map.get(map, path)
+                  new_dirs = [dir_path | Map.get(curr_internal_map, "dirs")]
+                  new_internal_map = Map.put(curr_internal_map, "dirs", new_dirs)
+                  new_map = Map.put(map, path, new_internal_map)
+                  {path, new_map}
+                String.match?(command, ~r/\d+.*/) -> 
+                  [file_size, _] = String.split(command)
+                  curr_internal_map = Map.get(map, path)
+                  files = Map.get(curr_internal_map, "files") + String.to_integer(file_size)
+                  new_internal_map = Map.put(curr_internal_map, "files", files)
+                  new_map = Map.put(map, path, new_internal_map)
+                  {path, new_map}
+
+              end
+            end)
 
     # Calculando o tamanho de cada diretório e
     # fazendo a soma dos menores que 100000
-    tree
-    |> Enum.reduce(0, fn {_key, value}, sum ->
-      size = sum_directory(tree, value)
+    {_, current_tree} = input
+
+    current_tree
+    |> Enum.reduce(0, fn {key, _value}, sum ->
+      size = sum_directory(current_tree, key)
       cond do
         size <= 100000 -> sum + size
         true -> sum
@@ -33,14 +66,13 @@ defmodule Day07 do
   # - Calcula o tamanho de um diretório
   # incluindo os subdiretórios
   defp sum_directory(tree, current_dir) do
-      Enum.reduce(current_dir, 0,fn item, sum ->
-        [identifier, name] = String.split(item)
-        IO.inspect(identifier)
-        cond do
-          String.match?(identifier, ~r/\d+/) -> sum + String.to_integer(identifier)
-          true -> sum + sum_directory(tree, Map.get(tree, name))
-        end
-      end)
+
+    map = Map.get(tree, current_dir)
+    IO.inspect(map)
+    files = Map.get(map, "files") 
+    dirs = Map.get(map, "dirs")
+           |> Enum.reduce(0, fn curr_dir, sum -> sum_directory(tree, curr_dir) + sum end)
+    files + dirs
   end
 
 end
