df509e799d7d85742d72a845e8e17b91ec54e44d
felipe <felipemoreira1@gmail.com>
2022-12-09 18:39:32 -0300

    Dia 09 - Problema 01

--- day09/lib/day09.ex
+++ day09/lib/day09.ex
@@ -11,30 +11,69 @@ defmodule Day09 do
   def positions_visited do
 
     head_position = {0, 0}
+    tail = [{0, 0}]
 
-    File.read!("./input.txt")
+    {head_pos, tail_list} = File.read!("./input.txt")
     |> String.split("\n", trim: true)
     |> Enum.map(fn string_move -> 
       [move, str_step] = String.split(string_move)
       {move, String.to_integer(str_step)}
     end)
-    |> Enum.reduce(head_position, fn move, acc -> 
-      move_head(acc, move)
+    |> Enum.reduce({head_position, tail}, fn move, acc -> 
+      {h, t} = acc
+      move_head(h, t, move)
     end)
+    
+    number_of_visisted_pos = tail_list |> Enum.uniq |> length
+    {head_pos, tail_list |> hd, number_of_visisted_pos}
 
   end
 
+  # ======= Utilitários
+
   # - Move a cabeça da corda o numero
   # de passos informados na direção informada
-  def move_head(head_position, move) do
+  defp move_head(head_position, tail, move) do
 
     {direction, steps} = move
 
     Enum.to_list(1..steps)
-    |> Enum.reduce(head_position, fn _step, acc -> 
-      IO.inspect(acc)
-      move_one_step(acc, direction)
+    |> Enum.reduce({head_position, tail}, fn _step, acc -> 
+      {h, t} = acc
+      new_head = move_one_step(h, direction)
+      current_tail = t |> hd
+      cond do
+        distance_between_points(new_head, current_tail) > 1 -> 
+          new_tail = move_tail(new_head, current_tail)
+          {new_head, [new_tail | t]}
+        true -> {new_head, t}
+      end
     end)
+
+  end
+
+  # - Move a cauda da corda
+  defp move_tail(head_position, tail_position) do
+    {head_x, head_y} = head_position
+    {tail_x, tail_y} = tail_position
+
+    cond do
+
+      # mesma linha
+      head_x === tail_x and head_y < tail_y -> {tail_x, tail_y - 1}
+      head_x === tail_x and head_y > tail_y -> {tail_x, tail_y + 1}
+
+      # mesma coluna
+      head_x < tail_x and head_y === tail_y -> {tail_x - 1, tail_y}
+      head_x > tail_x and head_y === tail_y -> {tail_x + 1, tail_y}
+
+      # diagonais
+      head_x > tail_x and head_y > tail_y -> {tail_x + 1, tail_y + 1}
+      head_x < tail_x and head_y < tail_y -> {tail_x - 1, tail_y - 1}
+      head_x > tail_x and head_y < tail_y -> {tail_x + 1, tail_y - 1}
+      head_x < tail_x and head_y > tail_y -> {tail_x - 1, tail_y + 1}
+
+    end
 
   end
 
