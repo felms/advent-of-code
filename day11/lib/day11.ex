d97eaabe2f9e7043b9ed02ee2102abeec62efa7d
Felipe <felipemoreira1@gmail.com>
2022-12-11 18:39:54 -0200

    Dia 11 - Problema 02

--- day11/lib/day11.ex
+++ day11/lib/day11.ex
@@ -25,6 +25,28 @@ defmodule Day11 do
 
   end
 
+  # ======= Problema 02 - Nivel de 'monkey business'
+  # depois de 10000 rodadas e sem alivio
+  def part_02 do
+
+    # Faz o parse do input e gera a estrutura de dados inicial
+    monkeys = File.read!("./input.txt")
+    |> String.replace("\r", "")
+    |> String.split("\n\n")
+    |> Enum.map(fn line -> Monkeys.parse_monkey(line) end)
+    |> Enum.reduce(%{}, fn monkey, acc ->
+      {id, map} = monkey
+      Map.put(acc, id, map)
+    end)
+
+    Enum.reduce(0..9999, monkeys, fn _round, acc ->
+      execute_round_without_relief(acc)
+    end)
+    |> Enum.map(fn {_, monkey} -> monkey.inspected_items end)
+    |> Enum.sort(:desc) |> Enum.take(2) |> Enum.product
+
+  end
+
   # - Executa uma rodada para todos os macacos
   def execute_round(monkeys) do
 
@@ -33,6 +55,18 @@ defmodule Day11 do
 
     Enum.reduce(0..number_of_monkeys, monkeys, fn monkey_id, acc ->
       Monkeys.execute_round(acc, monkey_id)
+    end)
+
+  end
+
+ # - Executa uma rodada para todos os macacos
+ def execute_round_without_relief(monkeys) do
+
+    number_of_monkeys = monkeys |> Enum.count
+    number_of_monkeys = number_of_monkeys - 1
+
+    Enum.reduce(0..number_of_monkeys, monkeys, fn monkey_id, acc ->
+      Monkeys.execute_round_without_relief(acc, monkey_id)
     end)
 
   end
--- day11/lib/monkeys.ex
+++ day11/lib/monkeys.ex
@@ -24,8 +24,8 @@ defmodule Monkeys do
 
   end
 
-  # - Executa a rodada para o macaco e retorna o macaco atualizado
-  # e duas listas com os itens a serem repassados a outros macacos
+  # - Executa a rodada para o macaco e retorna o 'Map'
+  # dos macacos atualizados
   def execute_round(monkeys, monkey_id) do
 
     monkey = Map.get(monkeys, monkey_id)
@@ -70,6 +70,72 @@ defmodule Monkeys do
     old_if_false_monkey = Map.get(monkeys, if_false_monkey)
 
     if_true_monkey_items = old_if_true_monkey.starting_items
+    if_false_monkey_items = old_if_false_monkey.starting_items
+
+    new_itm_items = if_true_monkey_items ++ Map.get(to_update, if_true_monkey)
+    new_if_true_monkey = Map.put(old_if_true_monkey, :starting_items, new_itm_items)
+
+    new_ifm_items = if_false_monkey_items ++ Map.get(to_update, if_false_monkey)
+    new_if_false_monkey = Map.put(old_if_false_monkey, :starting_items, new_ifm_items)
+
+    Map.put(monkeys, monkey_id, new_monkey)
+    |> Map.put(if_true_monkey, new_if_true_monkey)
+    |> Map.put(if_false_monkey, new_if_false_monkey)
+
+  end
+
+
+  # - Executa a rodada para o macaco e retorna o 'Map'
+  # dos macacos atualizado no caso onde o alivio é 1
+  def execute_round_without_relief(monkeys, monkey_id) do
+
+    monkey = Map.get(monkeys, monkey_id)
+
+    inspected_items = monkey.inspected_items
+    div_by = monkey.monkey_test.div_by
+    if_true_monkey = monkey.monkey_test.if_true;
+    if_false_monkey = monkey.monkey_test.if_false;
+    operation = monkey.operation
+
+    # Para manter os números dentro de uma faixa aceitável
+    common_divisor = Enum.map(monkeys, fn {_k, value} -> value.monkey_test.div_by end)
+    |> Enum.product
+    starting_items = monkey.starting_items |> Enum.map(fn item -> rem(item, common_divisor) end)
+
+
+    {inspected, to_update} =
+      starting_items
+      |> Enum.reduce({inspected_items, %{if_true_monkey => [], if_false_monkey => []}},
+        fn item, acc ->
+
+          {insp, map} = acc
+          new_value = apply_operation(operation, item)
+          new_inspected = insp + 1
+
+          cond do
+            rem(new_value, div_by) === 0 ->
+              new_if_true_list = Map.get(map, if_true_monkey) |> List.insert_at(-1, new_value)
+              new_map = Map.put(map, if_true_monkey, new_if_true_list)
+              {new_inspected, new_map}
+            true ->
+              new_if_false_list = Map.get(map, if_false_monkey) |> List.insert_at(-1, new_value)
+              new_map = Map.put(map, if_false_monkey, new_if_false_list)
+              {new_inspected, new_map}
+          end
+
+        end)
+
+    new_monkey = %{:inspected_items => inspected,
+      :monkey_test => monkey.monkey_test,
+      :operation => monkey.operation,
+      :starting_items => []}
+
+
+    old_if_true_monkey = Map.get(monkeys, if_true_monkey)
+    old_if_false_monkey = Map.get(monkeys, if_false_monkey)
+
+    if_true_monkey_items = old_if_true_monkey.starting_items
+
     if_false_monkey_items = old_if_false_monkey.starting_items
 
     new_itm_items = if_true_monkey_items ++ Map.get(to_update, if_true_monkey)
