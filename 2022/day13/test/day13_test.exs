962ddc7045f4e7176dfe996a208df544abb3dbf3
Felipe <felipemoreira1@gmail.com>
2022-12-15 12:37:28 -0200

    Dia 13 - Testes para a comparação das listas (fix - agora vai)

--- 2022/day13/test/day13_test.exs
+++ 2022/day13/test/day13_test.exs
@@ -21,20 +21,20 @@ defmodule Day13Test do
     assert Day13.sorter([9], [[8,7,6]]) == false
   end
 
-  test "sort almost equal lists"
+  test "sort almost equal lists" do
     assert Day13.sorter([[4,4],4,4], [[4,4],4,4,4]) == true
     assert Day13.sorter([7,7,7,7], [7,7,7]) == false
   end
 
-  test "sort non-empty vs empty list"
+  test "sort non-empty vs empty list" do
     assert Day13.sorter([], [3]) == true
   end
 
-  test "sort two nested empty lists"
+  test "sort two nested empty lists" do
     assert Day13.sorter([[[]]], [[]]) == false
   end
 
-  test "sort two deeply-nested empty lists"
+  test "sort two deeply-nested empty lists" do
     assert Day13.sorter([1,[2,[3,[4,[5,6,7]]]],8,9], [1,[2,[3,[4,[5,6,0]]]],8,9]) == false
   end
 end
