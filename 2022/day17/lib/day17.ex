5ddb6e5349a91980fb45dc6dd8a9877295df9b3e
Felipe <felipemoreira1@gmail.com>
2023-01-02 16:51:52 -0200

    Dia 17 - movimento vertical e horizontal

--- 2022/day17/lib/day17.ex
+++ 2022/day17/lib/day17.ex
@@ -20,7 +20,14 @@ defmodule Day17 do
 
     initial_grid = MapSet.new([{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}])
 
-    drop_rock(rocks.horizontal, initial_grid)
+    {_new_rock, new_grid, _new_move, new_jets} =
+      drop_rock(rocks.horizontal, initial_grid, jets)
+
+    {_, res, _, _} =
+      drop_rock(rocks.cross, new_grid, new_jets)
+
+    res
+
   end
 
   def position_rock(rock, grid) do
@@ -41,32 +48,63 @@ defmodule Day17 do
     |> elem(0)
   end
 
-  def drop_rock(rock, grid) do
+  def drop_rock(rock, grid, jets) do
 
     rock_pos = position_rock(rock, grid)
     highest_row = grid_highest_row(grid)
+    move = :horizontal
 
-    Enum.reduce_while(0..100, {rock_pos, grid}, fn _, acc ->
-      {curr_rock, curr_grid} = acc
-
-      IO.inspect(curr_rock)
+    Enum.reduce_while(0..100, {rock_pos, grid, move, jets}, fn _, acc ->
+      {curr_rock, curr_grid, curr_move, curr_jets} = acc
 
       lowest_row =
         curr_rock
         |> List.keysort(0)
         |> hd() |> elem(0)
 
-      if lowest_row - 1 === highest_row do
+      if lowest_row - 1 === highest_row and curr_move === :vertical do
         new_grid = MapSet.union(grid, MapSet.new(curr_rock))
-        {:halt, {curr_rock, new_grid}}
+        {:halt, {curr_rock, new_grid, curr_move, curr_jets}}
       else
-        new_rock =
-          curr_rock |> Enum.map(fn {row, col} -> {row - 1, col} end)
-        {:cont, {new_rock, curr_rock}}
+        {new_rock, new_move, new_jets} = move_rock(curr_rock, curr_move, curr_jets)
+        {:cont, {new_rock, curr_grid, new_move, new_jets}}
       end
 
     end)
 
   end
 
+  def move_rock(rock, move, jets) do
+    if move === :vertical do
+      new_rock = rock |> Enum.map(fn {row, col} -> {row - 1, col} end)
+      new_move = :horizontal
+      {new_rock, new_move, jets}
+    else
+      {direction, remaining_jets} = List.pop_at(jets, 0)
+      new_jets = List.insert_at(remaining_jets, -1, direction)
+      new_move = :vertical
+      new_rock = lateral_move(rock, direction)
+      {new_rock, new_move, new_jets}
+    end
+  end
+
+  def lateral_move(rock, direction) do
+
+    cond do
+      direction === "<" and leftmost_column(rock) > 0 ->
+        rock |> Enum.map(fn {row, column} -> {row, column - 1} end)
+      direction === ">" and rightmost_column(rock) < 6 ->
+        rock |> Enum.map(fn {row, column} -> {row, column + 1} end)
+      true -> rock
+    end
+
+  end
+
+  def leftmost_column(rock) do
+    rock |> List.keysort(1) |> hd() |> elem(1)
+  end
+
+  def rightmost_column(rock) do
+    rock |> List.keysort(1, :desc) |> hd() |> elem(1)
+  end
 end
