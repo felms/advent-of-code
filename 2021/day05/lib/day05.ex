a70b25f3661e24e7cd813f1ed188d45177744325
Felipe <felipemoreira1@gmail.com>
2023-03-13 14:43:25 -0300

    2021: Dia 05 - Problema 02

--- 2021/day05/lib/day05.ex
+++ 2021/day05/lib/day05.ex
@@ -18,12 +18,12 @@ defmodule Day05 do
         "\nCalculated in #{time / 1_000_000} seconds\n"
     )
 
-    # {time, result} = :timer.tc(&part_02/1, [input])
+    {time, result} = :timer.tc(&part_02/1, [input])
 
-    # IO.puts(
-    #   "==Part 02== \nResult: #{result}" <>
-    #     "\nCalculated in #{time / 1_000_000} seconds\n"
-    # )
+    IO.puts(
+      "==Part 02== \nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
   end
 
   defp parse_input(file) do
@@ -43,6 +43,35 @@ defmodule Day05 do
     |> Enum.filter(fn {_k, v} -> v >= 2 end)
     |> length()
   end
+
+  defp part_02(input) do
+    input
+    |> Enum.map(&parse_segment/1)
+    |> Enum.map(&segment/1)
+    |> List.flatten()
+    |> Enum.frequencies()
+    |> Enum.to_list()
+    |> Enum.filter(fn {_k, v} -> v >= 2 end)
+    |> length()
+  end
+
+  defp segment([[x1, y1], [x2, y2]] = input) do
+    cond do
+      x1 == x2 or y1 == y2 -> create_segment(input)
+      x1 == y1 and x2 == y2 -> main_diagonal(input, [])
+      abs(x1 - y1) == abs(x2 - y2) -> main_diagonal(input, [])
+      true -> secondary_diagonal(input, [])
+    end
+  end
+
+  defp main_diagonal([[x1, y1], [x2, y2]], acc) when (x1 == x2) and (y1 == y2), do: [{x1, y1} | acc]
+  defp main_diagonal([[x1, y1], [x2, y2]], acc) when (x1 < x2) and (y1 < y2), do: main_diagonal([[x1 + 1, y1 + 1], [x2, y2]], [{x1, y1} | acc])
+  defp main_diagonal([[x1, y1], [x2, y2]], acc) when (x1 > x2) and (y1 > y2), do: main_diagonal([[x1 - 1, y1 - 1], [x2, y2]], [{x1, y1} | acc])
+  defp main_diagonal([[x1, y1], [x2, y2]], acc) when (x1 > x2) and (y1 < y2), do: main_diagonal([[x1 - 1, y1 + 1], [x2, y2]], [{x1, y1} | acc])
+
+  defp secondary_diagonal([[x1, y1], [x2, y2]], acc) when (x1 == x2) and (y1 == y2), do: [{x1, y1} | acc]
+  defp secondary_diagonal([[x1, y1], [x2, y2]], acc) when (x1 < x2) and (y1 > y2), do: secondary_diagonal([[x1 + 1, y1 - 1], [x2, y2]], [{x1, y1} | acc])
+  defp secondary_diagonal([[x1, y1], [x2, y2]], acc) when (x1 > x2) and (y1 < y2), do: secondary_diagonal([[x1 - 1, y1 + 1], [x2, y2]], [{x1, y1} | acc])
 
   defp create_segment([[x1, y1], [x2, y2]]), do: for(x <- x1..x2, y <- y1..y2, do: {x, y})
 
