562689d4f672ec4242cd134fff3e9fbda5eb61cb
Felipe <felipemoreira1@gmail.com>
2023-03-20 09:34:32 -0300

    2021: Dia 10 - Problema 01

---
+++ 2021/day10/lib/day10.ex
@@ -0,0 +1,60 @@
+defmodule Day10 do
+  @moduledoc """
+  Dia 10 do Advent of Code de 2021
+  """
+
+  @error_score %{")" => 3, "]" => 57, "}" => 1197, ">" => 25137}
+
+  # Roda o problema no modo correto (teste ou real)
+  def run(:sample), do: solve("sample_input.txt")
+  def run(:real), do: solve("input.txt")
+
+  # Chama o método para a solução do problema
+  defp solve(input_file) do
+    input = parse_input(input_file)
+
+    {time, result} = :timer.tc(&part_01/1, [input])
+
+    IO.puts(
+      "==Part 01== \nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
+
+    # {time, result} = :timer.tc(&part_02/1, [input])
+
+    # IO.puts(
+    #   "==Part 02== \nResult: #{result}" <>
+    #     "\nCalculated in #{time / 1_000_000} seconds\n"
+    # )
+  end
+
+  defp parse_input(file) do
+    File.read!(file)
+    # Para evitar problemas no Windows
+    |> String.replace("\r", "")
+    |> String.split("\n", trim: true)
+  end
+
+  # ======= Problema 01
+  defp part_01(input) do
+    input
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.map(&(check_line(&1, [])))
+    |> Enum.filter(fn {status, _char} -> status == :corrupted end)
+    |> Enum.map(fn {_, char} -> @error_score[char] end)
+    |> Enum.sum()
+  end
+
+  defp check_line([], _stack), do: {:ok, ""}
+  defp check_line([current_char | chars], stack) when current_char in ["(", "[", "{", "<"], do: check_line(chars, [current_char | stack])
+  defp check_line([")" | chars], ["(" | stack]), do: check_line(chars, stack)
+  defp check_line(["]" | chars], ["[" | stack]), do: check_line(chars, stack)
+  defp check_line(["}" | chars], ["{" | stack]), do: check_line(chars, stack)
+  defp check_line([">" | chars], ["<" | stack]), do: check_line(chars, stack)
+  defp check_line([current_char | _chars], _stack), do: {:corrupted, current_char}
+
+  # ======= Problema 02
+  defp part_02(input) do
+  end
+
+end
