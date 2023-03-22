40c900d26e4504f00f4257fdfb92d4e12bdcf705
Felipe <felipemoreira1@gmail.com>
2023-03-22 14:27:24 -0300

    2022: Dia 16 - Parse do input

--- 2022/day16/lib/day16.ex
+++ 2022/day16/lib/day16.ex
@@ -1,6 +1,6 @@
 defmodule Day16 do
   @moduledoc """
-  Dia 16 do Advento of Code de 2022
+  Dia 16 do Advent of Code de 2022
   """
   def part_01(mode \\ :test) do
     case mode do
@@ -11,9 +11,6 @@ defmodule Day16 do
 
   defp run(input_file) do
     graph = parse_input(input_file)
-
-    dists = Tunnels.calc_distances(graph)
-    Tunnels.pressure_release(graph, 30, "AA", dists, [])
   end
 
   # - Faz o parse do input e gera uma tabela com
@@ -25,38 +22,26 @@ defmodule Day16 do
     # Para evitar problemas no Windows
     |> String.replace("\r", "")
     |> String.split("\n", trim: true)
-    |> Enum.reduce(%{}, fn scan, acc ->
-      [valve_string, tunnels_string] = String.split(scan, ";", trim: true)
-
-      valve = parse_valve(valve_string)
-      valve_name = Map.get(valve, "valve")
-      flow_rate = Map.get(valve, "flow_rate") |> String.to_integer()
-      tunnels = parse_tunnels(tunnels_string)
-
-      curr_valve = %{
-        flow_rate: flow_rate,
-        tunnels: tunnels
-      }
-
-      Map.put(acc, valve_name, curr_valve)
-    end)
+    |> Enum.map(&parse_valve/1)
+    |> Enum.into(%{})
   end
 
   # - Parse de uma 'válvula' do input separando em
   # nome e taxa de fluxo
   defp parse_valve(valve_string) do
+    %{"name" => name, "flow_rate" => flow_rate, "tunnels" => tunnels} = 
     Regex.named_captures(
-      ~r/Valve (?<valve>[A-Z]{2}) has flow rate=(?<flow_rate>\d+)/,
+      ~r/Valve (?<name>[A-Z]{2}) has flow rate=(?<flow_rate>\d+); (?<tunnels>tunnels?.*)/,
       valve_string
     )
+    {name, %{flow_rate: flow_rate, tunnels: parse_tunnels(tunnels)}}
   end
 
   # - Cria a lista com os túneis acessiveis
   # apartir do ponto atual
   defp parse_tunnels(tunnels_string) do
     tunnels_string
-    |> String.replace(~r/.*(valve |valves )/, "")
-    |> String.replace(" ", "")
-    |> String.split(",", trim: true)
+    |> String.replace(~r/tunnels? leads? to valves? /, "")
+    |> String.split(", ", trim: true)
   end
 end
