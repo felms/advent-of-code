1f671894e3a80e610b73b37ec492461d910436bd
Felipe <felipemoreira1@gmail.com>
2023-04-14 11:49:02 -0300

    2021: Dia 12 - parse do input

---
+++ 2021/day12/lib/day12.ex
@@ -0,0 +1,57 @@
+defmodule Day12 do
+  @moduledoc """
+  Dia 12 do Advent of Code de 2021 
+  """
+
+  # Roda o problema no modo correto (teste ou real)
+  def run(:small), do: solve("small_input.txt")
+  def run(:mid), do: solve("mid_input.txt")
+  def run(:sample), do: solve("sample_input.txt")
+  def run(:real), do: solve("input.txt")
+
+  # Chama o método para a solução do problema
+  defp solve(input_file) do
+    input = parse_input(input_file)
+
+    # {time, result} = :timer.tc(&part_01/1, [input])
+
+    # IO.puts(
+    #   "==Part 01== \nResult:\n#{result}" <>
+    #     "\nCalculated in #{time / 1_000_000} seconds\n"
+    # )
+
+    # {time, result} = :timer.tc(&part_02/1, [input])
+
+    # IO.puts(
+    #   "==Part 02== \nResult: #{result}" <>
+    #     "\nCalculated in #{time / 1_000_000} seconds\n"
+    # )
+  end
+
+  defp parse_input(file) do
+    File.read!(file)
+    # Para evitar problemas no Windows
+    |> String.replace("\r", "")
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_entry/1)
+    |> create_graph()
+  end
+
+  defp parse_entry(entry) do
+    [from, to] = String.split(entry, "-")
+    {from, to}
+  end
+
+  defp create_graph(entries), do: create_graph(entries, %{})
+  defp create_graph([], graph), do: graph
+
+  defp create_graph([{from, to} | entries], graph) do
+    from_tunnels = Map.get(graph, from, [])
+    to_tunnels = Map.get(graph, to, [])
+
+    create_graph(
+      entries,
+      Map.put(graph, from, [to | from_tunnels]) |> Map.put(to, [from | to_tunnels])
+    )
+  end
+end
