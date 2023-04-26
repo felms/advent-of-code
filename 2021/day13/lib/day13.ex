fe0170ac5f1e92e71e60d7be0498c6f6df8ae926
Felipe <felipemoreira1@gmail.com>
2023-04-26 13:12:23 -0300

    2021: Dia 13 - Problema 02

--- 2021/day13/lib/day13.ex
+++ 2021/day13/lib/day13.ex
@@ -18,12 +18,12 @@ defmodule Day13 do
         "\nCalculated in #{time / 1_000_000} seconds\n"
     )
 
-    # {time, result} = :timer.tc(&part_02/1, [input])
+    {time, result} = :timer.tc(&part_02/1, [input])
 
-    # IO.puts(
-    #   "==Part 02== \nResult: #{result}" <>
-    #     "\nCalculated in #{time / 1_000_000} seconds\n"
-    # )
+    IO.puts(
+      "==Part 02== \nResult: \n\n#{result}" <>
+        "\n\nCalculated in #{time / 1_000_000} seconds\n"
+    )
   end
 
   defp parse_input(file) do
@@ -35,13 +35,13 @@ defmodule Day13 do
 
     dots_set =
       dots
-      |> String.split("\n")
+      |> String.split("\n", trim: true)
       |> Enum.map(&parse_dot/1)
       |> MapSet.new()
 
     folds =
       instructions
-      |> String.split("\n")
+      |> String.split("\n", trim: true)
       |> Enum.map(&parse_fold/1)
 
     {dots_set, folds}
@@ -57,6 +57,27 @@ defmodule Day13 do
     |> String.replace("fold along ", "")
   end
 
+  # ======= Problema 01
+  defp part_01({points, folds}) do
+    execute_fold(points, hd(folds))
+    |> MapSet.size()
+  end
+
+  # ======= Problema 02
+  defp part_02({points, folds}) do
+    execute_folds(points, folds)
+    |> print_dots()
+  end
+
+  # ======= Utilitários
+
+  defp execute_folds(points, []), do: points
+  defp execute_folds(points, [fold | remaining_folds]) do
+    points
+    |> execute_fold(fold)
+    |> execute_folds(remaining_folds)
+  end
+
   defp execute_fold(points, fold) do
     cond do
       fold =~ ~r/x/ -> fold_x(points, String.replace(fold, "x=", "") |> String.to_integer())
@@ -67,7 +88,7 @@ defmodule Day13 do
   defp fold_x(points, x_value) do
     points
     |> MapSet.to_list()
-    |> Enum.map(fn {x, y} -> 
+    |> Enum.map(fn {x, y} ->
       new_x = if x > x_value, do: 2 * x_value - x, else: x
       {new_x, y}
     end)
@@ -77,16 +98,50 @@ defmodule Day13 do
   defp fold_y(points, y_value) do
     points
     |> MapSet.to_list()
-    |> Enum.map(fn {x, y} -> 
+    |> Enum.map(fn {x, y} ->
       new_y = if y > y_value, do: 2 * y_value - y, else: y
       {x, new_y}
     end)
     |> MapSet.new()
   end
 
-  # ======= Problema 01
-  defp part_01({points, folds}) do
-    execute_fold(points, hd(folds))
-    |> MapSet.size()
+  defp print_dots(points) do
+    points
+    |> get_maxes()
+    |> generate_grid()
+    |> Enum.map(&print_row(&1, points))
+    |> Enum.join("\n")
+  end
+
+  defp get_maxes(points) do
+    points_list = MapSet.to_list(points)
+
+    max_x =
+      points_list
+      |> Enum.map(fn {x, _y} -> x end)
+      |> Enum.max()
+
+    max_y =
+      points_list
+      |> Enum.map(fn {_x, y} -> y end)
+      |> Enum.max()
+
+    {max_x, max_y}
+  end
+
+  defp generate_grid({max_x, max_y}) do
+    grid = for x <- 0..max_x, y <- 0..max_y, do: {x, y}
+
+    grid
+    |> Enum.group_by(fn {_x, y} -> y end)
+    |> Enum.map(fn {_k, v} -> v end)
+  end
+
+  defp print_row(row, points) do
+    row
+    |> Enum.map(fn point ->
+      if point in points, do: "#", else: "."
+    end)
+    |> Enum.join(" ")
   end
 end
