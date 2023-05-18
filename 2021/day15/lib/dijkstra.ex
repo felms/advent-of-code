655031bd1ab2ef5a37b2f5ba5beb223cab980a64
felipe <felipemoreira1@gmail.com>
2023-05-18 20:30:17 -0300

    2021: Dia 15 - comentários

--- 2021/day15/lib/dijkstra.ex
+++ 2021/day15/lib/dijkstra.ex
@@ -1,8 +1,19 @@
 defmodule Dijkstra do
+  @moduledoc """
+  Implementa o Algoritmo de Dijkstra.
+  """
+
+  @type priority_queue :: {heap :: list(), size :: integer()}
+
   defguard is_empty(pqueue) when pqueue.size == 0
 
   @infinity 1_000_000_000
 
+  @doc """
+  Gera um mapa com a mínima distancia de source a todos
+  os outros pontos do grafo.
+  """
+  @spec find_min_distances(graph :: map(), source :: tuple()) :: map()
   def find_min_distances(graph, source) do
     dist =
       graph
@@ -16,10 +27,11 @@ defmodule Dijkstra do
     find_min_distances(graph, pqueue, dist)
   end
 
-  defp find_min_distances(graph, pqueue, dist) when is_empty(pqueue), do: dist
+  @spec find_min_distances(graph :: map(), pqueue :: priority_queue(), dists :: map()) :: map()
+  defp find_min_distances(_graph, pqueue, dist) when is_empty(pqueue), do: dist
 
   defp find_min_distances(graph, pqueue, dist) do
-    {updated_pqueue, {u, u_priority}} = PQueue.poll(pqueue)
+    {updated_pqueue, {u, _u_priority}} = PQueue.poll(pqueue)
     neighbors = find_neighbors(graph, u)
 
     {new_queue, new_dist} = process_neighbors(graph, updated_pqueue, dist, u, neighbors)
@@ -27,6 +39,15 @@ defmodule Dijkstra do
     find_min_distances(graph, new_queue, new_dist)
   end
 
+  # -- Faz a função do 'for' no Algoritmo de Dijkstra.
+  # -- Processa todos os vizinhos de um ponto.
+  @spec process_neighbors(
+          graph :: map(),
+          pqueue :: priority_queue(),
+          dist :: map(),
+          point :: tuple(),
+          neighbors :: list()
+        ) :: tuple()
   defp process_neighbors(_graph, pqueue, dist, _point, []), do: {pqueue, dist}
 
   defp process_neighbors(graph, pqueue, dist, point, [v | neighbors]) do
@@ -41,6 +62,8 @@ defmodule Dijkstra do
     end
   end
 
+  # -- Encontra todos os vizinhos de um ponto.
+  @spec find_neighbors(graph :: map(), point :: tuple()) :: list()
   defp find_neighbors(graph, {x, y}),
     do: [{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}] |> Enum.filter(&Map.has_key?(graph, &1))
 end
