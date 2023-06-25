908c3bb70549ca15b6d07c99fea1c2cb9d5183ba
felipe <felipemoreira1@gmail.com>
2023-06-24 23:08:01 -0300

    2015: Dia 03 - Problema 02

--- 2015/day03/lib/day03.ex
+++ 2015/day03/lib/day03.ex
@@ -13,12 +13,12 @@ defmodule Day03 do
         "\nCalculated in #{time / 1_000_000} seconds\n"
     )
 
-    # {time, result} = :timer.tc(&part_02/1, [input])
+    {time, result} = :timer.tc(&part_02/1, [input])
 
-    # IO.puts(
-    #   "==Part 02== \nResult: \n\n#{result}" <>
-    #     "\nCalculated in #{time / 1_000_000} seconds\n"
-    # )
+    IO.puts(
+      "==Part 02== \nResult: \n#{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
   end
 
   defp parse_input(file) do
@@ -37,14 +37,158 @@ defmodule Day03 do
   end
 
   defp visit([], _current_pos, visited), do: visited
-  defp visit([">" | directions], {x, y}, visited), do: visit(directions, {x + 1, y}, MapSet.put(visited, {x + 1, y}))
-  defp visit(["v" | directions], {x, y}, visited), do: visit(directions, {x, y - 1}, MapSet.put(visited, {x, y - 1}))
-  defp visit(["<" | directions], {x, y}, visited), do: visit(directions, {x - 1, y}, MapSet.put(visited, {x - 1, y}))
-  defp visit(["^" | directions], {x, y}, visited), do: visit(directions, {x, y + 1}, MapSet.put(visited, {x, y + 1}))
+
+  defp visit([">" | directions], {x, y}, visited),
+    do: visit(directions, {x + 1, y}, MapSet.put(visited, {x + 1, y}))
+
+  defp visit(["v" | directions], {x, y}, visited),
+    do: visit(directions, {x, y - 1}, MapSet.put(visited, {x, y - 1}))
+
+  defp visit(["<" | directions], {x, y}, visited),
+    do: visit(directions, {x - 1, y}, MapSet.put(visited, {x - 1, y}))
+
+  defp visit(["^" | directions], {x, y}, visited),
+    do: visit(directions, {x, y + 1}, MapSet.put(visited, {x, y + 1}))
 
   # ======= Problema 02
-  # defp part_02(input) do
-  #   input
-  # end
-end
+  defp part_02(input) do
+    input
+    |> visit_rs(
+      {0, 0},
+      {0, 0},
+      :santa,
+      MapSet.put(MapSet.new(), {0, 0})
+    )
+    |> MapSet.size()
+  end
 
+  defp visit_rs([], _santa_pos, _robot_santa_pos, _turn, visited), do: visited
+
+  defp visit_rs(
+         [">" | directions],
+         {x, y},
+         robot_santa_pos,
+         :santa,
+         visited
+       ),
+       do:
+         visit_rs(
+           directions,
+           {x + 1, y},
+           robot_santa_pos,
+           :robot_santa,
+           MapSet.put(visited, {x + 1, y})
+         )
+
+  defp visit_rs(
+         ["v" | directions],
+         {x, y},
+         robot_santa_pos,
+         :santa,
+         visited
+       ),
+       do:
+         visit_rs(
+           directions,
+           {x, y - 1},
+           robot_santa_pos,
+           :robot_santa,
+           MapSet.put(visited, {x, y - 1})
+         )
+
+  defp visit_rs(
+         ["<" | directions],
+         {x, y},
+         robot_santa_pos,
+         :santa,
+         visited
+       ),
+       do:
+         visit_rs(
+           directions,
+           {x - 1, y},
+           robot_santa_pos,
+           :robot_santa,
+           MapSet.put(visited, {x - 1, y})
+         )
+
+  defp visit_rs(
+         ["^" | directions],
+         {x, y},
+         robot_santa_pos,
+         :santa,
+         visited
+       ),
+       do:
+         visit_rs(
+           directions,
+           {x, y + 1},
+           robot_santa_pos,
+           :robot_santa,
+           MapSet.put(visited, {x, y + 1})
+         )
+
+  defp visit_rs(
+         [">" | directions],
+         santa_pos,
+         {x, y},
+         :robot_santa,
+         visited
+       ),
+       do:
+         visit_rs(
+           directions,
+           santa_pos,
+           {x + 1, y},
+           :santa,
+           MapSet.put(visited, {x + 1, y})
+         )
+
+  defp visit_rs(
+         ["v" | directions],
+         santa_pos,
+         {x, y},
+         :robot_santa,
+         visited
+       ),
+       do:
+         visit_rs(
+           directions,
+           santa_pos,
+           {x, y - 1},
+           :santa,
+           MapSet.put(visited, {x, y - 1})
+         )
+
+  defp visit_rs(
+         ["<" | directions],
+         santa_pos,
+         {x, y},
+         :robot_santa,
+         visited
+       ),
+       do:
+         visit_rs(
+           directions,
+           santa_pos,
+           {x - 1, y},
+           :santa,
+           MapSet.put(visited, {x - 1, y})
+         )
+
+  defp visit_rs(
+         ["^" | directions],
+         santa_pos,
+         {x, y},
+         :robot_santa,
+         visited
+       ),
+       do:
+         visit_rs(
+           directions,
+           santa_pos,
+           {x, y + 1},
+           :santa,
+           MapSet.put(visited, {x, y + 1})
+         )
+end
