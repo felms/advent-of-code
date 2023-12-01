ae2a95d157fa1a5e0d8c64af5ee8151eba57cf60
Felipe <felipemoreira1@gmail.com>
2023-12-01 14:24:37 -0200

    2023: Dia 01 - Parte 02

--- 2023/day01/lib/day01.ex
+++ 2023/day01/lib/day01.ex
@@ -12,13 +12,32 @@ defmodule Day01 do
       |> String.replace("\r", "")
       |> String.split("\n", trim: true)
 
-    part_01(input)
+    {time, result} = :timer.tc(&part_01/1, [input])
+
+    IO.puts(
+      "==Part 01== \nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
+
+    {time, result} = :timer.tc(&part_02/1, [input])
+
+    IO.puts(
+      "==Part 02== \nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
   end
 
   # - Resolve a parte 01 do problema
   def part_01(input) do
     input
     |> Enum.map(&get_calibration_value/1)
+    |> Enum.sum()
+  end
+
+  # - Resolve a parte 02 do problema
+  def part_02(input) do
+    input
+    |> Enum.map(&get_calibration_value_part_02/1)
     |> Enum.sum()
   end
 
@@ -32,5 +51,31 @@ defmodule Day01 do
 
     (List.first(digits) <> List.last(digits))
     |> String.to_integer()
+  end
+
+  # - Gera o número do valor de calibração
+  # com base na string de input também utilizando os
+  # digitos escritos.
+  def get_calibration_value_part_02(input_string) do
+    # Feio, mas foi uma maneira simples de se livrar
+    # dos "edge cases". Por exemplo: "sevenine", "eighthree", etc...
+    string_to_number = %{
+      "one" => "one1one",
+      "two" => "two2two",
+      "three" => "three3three",
+      "four" => "four4four",
+      "five" => "five5five",
+      "six" => "six6six",
+      "seven" => "seven7seven",
+      "eight" => "eight8eight",
+      "nine" => "nine9nine"
+    }
+
+    string_to_number
+    |> Map.keys()
+    |> Enum.reduce(input_string, fn mapping, acc ->
+      String.replace(acc, mapping, Map.get(string_to_number, mapping))
+    end)
+    |> get_calibration_value()
   end
 end
--- 2023/day01/test/day01_test.exs
+++ 2023/day01/test/day01_test.exs
@@ -19,4 +19,34 @@ defmodule Day01Test do
       assert Day01.get_calibration_value("treb7uchet") == 77
     end
   end
+
+  describe "tests Day01.get_calibration_value_part_02/1" do
+    test "tests two1nine" do
+      assert Day01.get_calibration_value_part_02("two1nine") == 29
+    end
+
+    test "tests eightwothree" do
+      assert Day01.get_calibration_value_part_02("eightwothree") == 83
+    end
+
+    test "tests abcone2threexyz" do
+      assert Day01.get_calibration_value_part_02("abcone2threexyz") == 13
+    end
+
+    test "tests xtwone3four" do
+      assert Day01.get_calibration_value_part_02("xtwone3four") == 24
+    end
+
+    test "tests 4nineeightseven2" do
+      assert Day01.get_calibration_value_part_02("4nineeightseven2") == 42
+    end
+
+    test "tests zoneight234" do
+      assert Day01.get_calibration_value_part_02("zoneight234") == 14
+    end
+
+    test "tests 7pqrstsixteen" do
+      assert Day01.get_calibration_value_part_02("7pqrstsixteen") == 76
+    end
+  end
 end
