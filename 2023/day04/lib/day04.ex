4ab53926a1952ec6f531eee21a323426e4469511
Felipe <felipemoreira1@gmail.com>
2023-12-04 12:29:55 -0200

    2023: Dia 04 - Problema 01

---
+++ 2023/day04/lib/day04.ex
@@ -0,0 +1,62 @@
+defmodule Day04 do
+  @moduledoc """
+  Dia 04 do Advent of Code 2023
+  """
+
+  def run(mode \\ :real_input) do
+    input_file = if mode == :sample, do: "sample_input.txt", else: "input.txt"
+
+    input =
+      File.read!(input_file)
+      # Para evitar problemas no Windows
+      |> String.replace("\r", "")
+      |> parse_input()
+
+    {time, result} = :timer.tc(&part_01/1, [input])
+
+    IO.puts(
+      "==Part 01== \nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
+
+    # {time, result} = :timer.tc(&part_02/1, [input])
+
+    # IO.puts(
+    #   "==Part 02== \nResult: #{result}" <>
+    #     "\nCalculated in #{time / 1_000_000} seconds\n"
+    # )
+  end
+
+  # Problema 01
+  def part_01(input) do
+    input
+    |> Enum.map(&card_points/1)
+    |> Enum.sum()
+  end
+
+  def card_points(card) do
+    matches =
+      card.card_numbers
+      |> Enum.count(fn number -> number in card.winning_numbers end)
+
+    if matches > 0, do: Integer.pow(2, matches - 1), else: 0
+  end
+
+  def parse_input(input_string) do
+    input_string
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_card/1)
+  end
+
+  def parse_card(input) do
+    [w, n] =
+      input
+      |> String.replace(~r/Card \d+:/, "")
+      |> String.split("|", trim: true)
+
+    %{
+      winning_numbers: w |> String.split(~r/\s+/, trim: true),
+      card_numbers: n |> String.split(~r/\s+/, trim: true)
+    }
+  end
+end
