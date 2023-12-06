9bbe4cdd1cc6ca963b75598b7c87e4cb635c867a
Felipe <felipemoreira1@gmail.com>
2023-12-06 14:59:57 -0200

    2023: Dia 05 - Problema 01

---
+++ 2023/day05/lib/day05.ex
@@ -0,0 +1,64 @@
+defmodule Day05 do
+  @moduledoc """
+  Dia 05 do Advent of Code 2023
+  """
+
+  def run(mode \\ :real_input) do
+    input_file = if mode == :sample, do: "sample_input.txt", else: "input.txt"
+
+    input =
+      File.read!(input_file)
+      # Para evitar problemas no Windows
+      |> String.replace("\r", "")
+      |> parse_input()
+
+    {time, result} = :timer.tc(&part_01/1, [input])
+
+    IO.puts(
+      "==Part 01== \nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
+
+    # {time, result} = :timer.tc(&part_02/1, [input])
+
+    # IO.puts(
+    #   "==Part 02== \nResult: #{result}" <>
+    #     "\nCalculated in #{time / 1_000_000} seconds\n"
+    # )
+  end
+
+  def part_01({seeds_list, conversion_maps}) do
+    [first_map] = conversion_maps |> Enum.filter(&(&1.from == "seed"))
+
+    seeds_list
+    |> Enum.map(fn seed -> map_to_location(seed, first_map, conversion_maps) end)
+    |> Enum.min()
+  end
+
+  def parse_input(input_data) do
+    [seed_input | mappings_input] = input_data |> String.split("\n\n", trim: true)
+
+    seeds_list =
+      seed_input
+      |> String.replace("seeds: ", "")
+      |> String.split(~r/\s+/, trim: true)
+      |> Enum.map(&String.to_integer/1)
+
+    conversion_maps =
+      mappings_input
+      |> Enum.map(&ConversionMap.new/1)
+
+    {seeds_list, conversion_maps}
+  end
+
+  def map_to_location(current_number, current_map, conversion_maps) do
+    if current_map.to == "location" do
+      ConversionMap.convert(current_map, current_number)
+    else
+      new_current_number = ConversionMap.convert(current_map, current_number)
+
+      [new_current_map] = conversion_maps |> Enum.filter(&(&1.from == current_map.to))
+      map_to_location(new_current_number, new_current_map, conversion_maps)
+    end
+  end
+end
