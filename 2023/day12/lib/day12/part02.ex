246dd0a7cbfce0a954983e3b9b5d7d94736c0bd8
Felipe <felipemoreira1@gmail.com>
2023-12-15 16:26:44 -0200

    2023: Dia 12 - Problema 02

--- 2023/day12/lib/day12/part02.ex
+++ 2023/day12/lib/day12/part02.ex
@@ -2,8 +2,16 @@ defmodule Day12.Part02 do
   @moduledoc """
   Parte 02 do dia 12 do Advent of Code 2023
   """
+  use Agent # Para usar o cache
+
+  def start_link() do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
 
   def run(mode \\ :real_input) do
+
+    start_link()
+
     input_file = if mode == :sample, do: "sample_input.txt", else: "input.txt"
 
     input =
@@ -12,17 +20,32 @@ defmodule Day12.Part02 do
       |> String.replace("\r", "")
       |> parse_input()
 
-    {time, result} = :timer.tc(&part_02/1, [input])
+    {time, result} = :timer.tc(&part_01/1, [input])
 
     IO.puts(
       "\n==Part 01== \n\nResult: #{result}" <>
         "\nCalculated in #{time / 1_000_000} seconds\n"
     )
+
+    {time, result} = :timer.tc(&part_02/1, [input])
+
+    IO.puts(
+      "\n==Part 02== \n\nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
+  end
+
+  # - Problema 01
+  def part_01(input) do
+    input
+    |> Enum.map(&count_possible_arrangements/1)
+    |> Enum.sum()
   end
 
   # - Problema 02
   def part_02(input) do
     input
+    |> Enum.map(&unfold/1)
     |> Enum.map(&count_possible_arrangements/1)
     |> Enum.sum()
   end
@@ -37,10 +60,18 @@ defmodule Day12.Part02 do
     [record, groups_string] = row |> String.split(~r/\s+/, trim: true)
 
     {
-      ("." <> record <> ".") |> String.graphemes(),
+      record |> String.graphemes(),
       groups_string
       |> String.split(",", trim: true)
       |> Enum.map(&String.to_integer/1)
+    }
+  end
+
+  # - "Desdobra" um registro
+  def unfold({record, groups}) do
+    {
+      record |> List.duplicate(5) |> Enum.intersperse(["?"]) |> List.flatten(),
+      groups |> List.duplicate(5) |> List.flatten()
     }
   end
 
@@ -62,7 +93,8 @@ defmodule Day12.Part02 do
 
   # - Conta o número de possíveis arranjos
   # que batem com o critério
-  def count_possible_arrangements({record, group}), do: count_possible_arrangements(record, group)
+  def count_possible_arrangements({record, group}),
+    do: cached_count(["."] ++ record ++ ["."], group)
 
   def count_possible_arrangements([], []), do: 1
   def count_possible_arrangements([], _group), do: 0
@@ -78,7 +110,7 @@ defmodule Day12.Part02 do
     |> Enum.reduce(0, fn pos, acc ->
       if fits?(record, pos, current_group) do
         acc +
-          count_possible_arrangements(
+          cached_count(
             ["." | record |> Enum.drop(pos + current_group + 1)],
             groups
           )
@@ -87,4 +119,23 @@ defmodule Day12.Part02 do
       end
     end)
   end
+
+  # - Conta o número de possíveis arranjos
+  # que batem com o critério utilizando um Dicionário
+  # como cache
+  def cached_count(record, group) do
+    key = "key:#{record |> List.to_string}-#{group |> List.to_string}"
+
+    res = Agent.get(__MODULE__, & (Map.get(&1, key)))
+
+    if res do
+      res
+    else
+      calculated_res = count_possible_arrangements(record, group)
+      Agent.update(__MODULE__, &(Map.put(&1, key, calculated_res)))
+      calculated_res
+    end
+  end
+
+
 end
