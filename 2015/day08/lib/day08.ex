0cbb5df6e3a270cd2858603973a33d749d901a22
felipe <felipemoreira1@gmail.com>
2024-01-04 20:16:32 -0300

    2015: Dia 08 - Problema 02

--- 2015/day08/lib/day08.ex
+++ 2015/day08/lib/day08.ex
@@ -18,6 +18,13 @@ defmodule Day08 do
       "\n==Part 01== \n\nResult: #{result}" <>
         "\nCalculated in #{time / 1_000_000} seconds\n"
     )
+
+    {time, result} = :timer.tc(&part02/1, [input])
+
+    IO.puts(
+      "\n==Part 02== \n\nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
   end
 
   # - Problema 01
@@ -25,6 +32,16 @@ defmodule Day08 do
     input
     |> Enum.map(&string_score/1)
     |> Enum.sum()
+  end
+
+  # - Problema 02
+  def part02(input) do
+    input
+    |> Enum.reduce(0, fn string, acc ->
+      val0 = string |> encode_string() |> characters_of_code()
+      val1 = string |> characters_of_code()
+      val0 - val1 + acc
+    end)
   end
 
   def string_score(string) do
@@ -54,5 +71,23 @@ defmodule Day08 do
 
   def characters_in_memory([_ | remaining_chars], _mode, count) do
     characters_in_memory(remaining_chars, :normal, count + 1)
+  end
+
+  def encode_string(string) do
+    encode_string(String.graphemes(string), "")
+  end
+
+  def encode_string([], str), do: "\"" <> str <> "\""
+
+  def encode_string(["\"" | remaining_chars], str) do
+    encode_string(remaining_chars, str <> "\\\"")
+  end
+
+  def encode_string(["\\" | remaining_chars], str) do
+    encode_string(remaining_chars, str <> "\\\\")
+  end
+
+  def encode_string([char | remaining_chars], str) do
+    encode_string(remaining_chars, str <> char)
   end
 end
