6b069d21e540c18a203ec6dd8740f12783a76dc8
felipe <felipemoreira1@gmail.com>
2024-01-25 17:34:43 -0300

    2015: Dia 09 - Problema 01

--- 2015/day09/lib/day09.ex
+++ 2015/day09/lib/day09.ex
@@ -12,11 +12,29 @@ defmodule Day09 do
       |> String.replace("\r", "")
       |> parse_input()
 
+    {time, result} = :timer.tc(&part01/1, [input])
+
+    IO.puts(
+      "\n==Part 01== \n\nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
   end
 
   # - Problema 01
   def part01(input) do
-    # TODO
+    cities =
+      input
+      |> Enum.map(fn {{from, to}, _v} -> [from, to] end)
+      |> List.flatten()
+      |> Enum.uniq()
+
+    0..((cities |> length()) - 1)
+    |> Enum.reduce([], fn index, acc ->
+      city = Enum.at(cities, index)
+      rem = List.delete_at(cities, index)
+      [min_dists([city | rem], input, 0) | acc]
+    end)
+    |> Enum.min()
   end
 
   def parse_input(input) do
@@ -24,11 +42,21 @@ defmodule Day09 do
     |> String.split("\n", trim: true)
     |> Enum.reduce([], fn distance, acc ->
       [_, from, to, dist] = Regex.run(~r/(.+) to (.+) = (\d+)/, distance)
-      [{{from, to}, String.to_integer(dist)} | acc]
+      [{{from, to}, String.to_integer(dist)}, {{to, from}, String.to_integer(dist)} | acc]
     end)
+    |> Map.new()
   end
 
-  def min_dists(curr_city, dists, total_dist) do
-    # TODO 
+  def min_dists([_curr_city | []], _dists, total_dist), do: total_dist
+
+  def min_dists([curr_city | rem_cities], dists, total_dist) do
+    0..((rem_cities |> length()) - 1)
+    |> Enum.reduce([], fn index, acc ->
+      selected_city = Enum.at(rem_cities, index)
+      key = {curr_city, selected_city}
+      new_dist = total_dist + Map.get(dists, key)
+      [min_dists([selected_city | List.delete_at(rem_cities, index)], dists, new_dist) | acc]
+    end)
+    |> Enum.min()
   end
 end
