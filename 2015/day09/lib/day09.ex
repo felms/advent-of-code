e084fa9d4e273f377ca587f16d199c7154d16de0
felipe <felipemoreira1@gmail.com>
2024-01-25 17:44:49 -0300

    2015: Dia 09 - Problema 02

--- 2015/day09/lib/day09.ex
+++ 2015/day09/lib/day09.ex
@@ -18,6 +18,13 @@ defmodule Day09 do
       "\n==Part 01== \n\nResult: #{result}" <>
         "\nCalculated in #{time / 1_000_000} seconds\n"
     )
+
+    {time, result} = :timer.tc(&part02/1, [input])
+
+    IO.puts(
+      "\n==Part 02== \n\nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
   end
 
   # - Problema 01
@@ -36,6 +43,25 @@ defmodule Day09 do
     end)
     |> Enum.min()
   end
+  
+
+  # - Problema 02
+  def part02(input) do
+    cities =
+      input
+      |> Enum.map(fn {{from, to}, _v} -> [from, to] end)
+      |> List.flatten()
+      |> Enum.uniq()
+
+    0..((cities |> length()) - 1)
+    |> Enum.reduce([], fn index, acc ->
+      city = Enum.at(cities, index)
+      rem = List.delete_at(cities, index)
+      [max_dists([city | rem], input, 0) | acc]
+    end)
+    |> Enum.max()
+  end
+
 
   def parse_input(input) do
     input
@@ -59,4 +85,18 @@ defmodule Day09 do
     end)
     |> Enum.min()
   end
+  
+  def max_dists([_curr_city | []], _dists, total_dist), do: total_dist
+
+  def max_dists([curr_city | rem_cities], dists, total_dist) do
+    0..((rem_cities |> length()) - 1)
+    |> Enum.reduce([], fn index, acc ->
+      selected_city = Enum.at(rem_cities, index)
+      key = {curr_city, selected_city}
+      new_dist = total_dist + Map.get(dists, key)
+      [max_dists([selected_city | List.delete_at(rem_cities, index)], dists, new_dist) | acc]
+    end)
+    |> Enum.max()
+  end
+
 end
