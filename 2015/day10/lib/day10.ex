9f15782522c6f9426d920708a88b96c8c9b07985
felipe <felipemoreira1@gmail.com>
2024-01-27 17:56:58 -0300

    2015: Dia 10 - Problema 01 (little refactor)

--- 2015/day10/lib/day10.ex
+++ 2015/day10/lib/day10.ex
@@ -17,12 +17,27 @@ defmodule Day10 do
       "\n==Part 01== \n\nResult: #{result}" <>
         "\nCalculated in #{time / 1_000_000} seconds\n"
     )
+
+    # {time, result} = :timer.tc(&part02/1, [input])
+
+    # IO.puts(
+    #   "\n==Part 02== \n\nResult: #{result}" <>
+    #     "\nCalculated in #{time / 1_000_000} seconds\n"
+    # )
   end
 
   # - Problema 01
   def part01(input) do
-      execute_steps(input |> String.trim() |> String.to_integer(), 40) 
-      |> Integer.to_string() |> String.length()
+    execute_steps(input |> String.trim() |> String.to_integer(), 40)
+    |> Integer.to_string()
+    |> String.length()
+  end
+
+  # - Problema 02
+  def part02(input) do
+    execute_steps(input |> String.trim() |> String.to_integer(), 50)
+    |> Integer.to_string()
+    |> String.length()
   end
 
   def execute_steps(input, number_of_steps) do
@@ -32,42 +47,30 @@ defmodule Day10 do
     end)
   end
 
-  # - Base case
-  def step(1), do: 11
-  # - Remaining cases
   def step(input) do
-    [digit | digits] = input |> Integer.digits()
-    step(digits, digit, []) |> Integer.undigits()
+    step(input |> Integer.digits(), [])
+    |> compress([])
   end
 
-  def step([head_digit | rem_digits], current_digit, []),
-    do: step(rem_digits, head_digit, [{current_digit, 1}])
+  def step([], digits), do: digits
+  def step([head | tail], []), do: step(tail, [{head, 1}])
 
-  def step([], current_digit, [h_counts | t_counts]) do
-    {digit, count} = h_counts
-
-    res_digits =
-      cond do
-        digit == current_digit -> [{digit, count + 1} | t_counts]
-        true -> [{current_digit, 1}, h_counts | t_counts]
-      end
-
-    res_digits |> Enum.reverse() |> collapse([])
+  def step([head | tail], digits) do
+    step(tail, combine_to_head(digits, head))
   end
 
-  def step([head_digit | tail_digits], current_digit, [h_counts | t_counts]) do
-    {digit, count} = h_counts
+  def combine_to_head([head | tail] = list, digit) do
+    {number, count} = head
 
     cond do
-      digit == current_digit -> step(tail_digits, head_digit, [{digit, count + 1} | t_counts])
-      true -> step(tail_digits, head_digit, [{current_digit, 1}, h_counts | t_counts])
+      number == digit -> [{number, count + 1} | tail]
+      true -> [{digit, 1} | list]
     end
   end
 
-  def collapse([], number), do: number
+  def compress([], number), do: number |> Integer.undigits()
 
-  def collapse([h | tail], number) do
-    {num, cnt} = h
-    collapse(tail, number ++ [cnt, num])
+  def compress([{digit, count} | tail], number) do
+    compress(tail, [count, digit | number])
   end
 end
