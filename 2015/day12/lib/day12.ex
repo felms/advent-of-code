9719dce88ebe80b6807fbc9fbe646fc0a5a0db37
felipe <felipemoreira1@gmail.com>
2024-10-13 18:52:57 -0300

    2015: Dia 12 - Problema 01

--- 2015/day12/lib/day12.ex
+++ 2015/day12/lib/day12.ex
@@ -1,18 +1,27 @@
 defmodule Day12 do
   @moduledoc """
-  Documentation for `Day12`.
+  Dia 12 do Advent of Code de 2015
   """
+  def run() do
+    input_file = "input.txt"
 
-  @doc """
-  Hello world.
+    input =
+      File.read!(input_file)
+      # Para evitar problemas no Windows
+      |> String.replace("\r", "")
 
-  ## Examples
+    {time, result} = :timer.tc(&part01/1, [input])
 
-      iex> Day12.hello()
-      :world
+    IO.puts(
+      "\n==Part 01== \n\nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
+  end
 
-  """
-  def hello do
-    :world
+  # - Problema 01
+  def part01(input) do
+    Regex.scan(~r/-?\d+/, input)
+    |> Enum.map(fn [x] -> String.to_integer(x) end)
+    |> Enum.sum
   end
 end
