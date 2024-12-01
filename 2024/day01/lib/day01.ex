5da966defe1b977c45ba31db42cb2ed3aff77bf8
felipe <felipemoreira1@gmail.com>
2024-12-01 19:09:22 -0300

    2014: Dia 01 - Problema 01

--- 2024/day01/lib/day01.ex
+++ 2024/day01/lib/day01.ex
@@ -1,18 +1,48 @@
 defmodule Day01 do
   @moduledoc """
-  Documentation for `Day01`.
+  Dia 01 do Advent of Code 2024
   """
 
-  @doc """
-  Hello world.
+  def run(mode \\ nil) do
+    input_file = if mode == :sample, do: "sample_input.txt", else: "input.txt"
 
-  ## Examples
+    input =
+      File.read!(input_file)
+      # Para evitar problemas no Windows
+      |> String.replace("\r", "")
+      |> parse_input()
 
-      iex> Day01.hello()
-      :world
+    {time, result} = :timer.tc(&part_01/1, [input])
 
-  """
-  def hello do
-    :world
+    IO.puts(
+      "\n==Part 01== \n\nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
+
+    #{time, result} = :timer.tc(&part_02/1, [input])
+
+    #IO.puts(
+    #  "\n==Part 02== \n\nResult: #{result}" <>
+    #    "\nCalculated in #{time / 1_000_000} seconds\n"
+    #)
+
   end
+
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.split/1)
+    |> Enum.reduce([[],[]], fn [a, b], [first_list, second_list] -> 
+      [[String.to_integer(a) | first_list], [String.to_integer(b) | second_list]]
+    end)
+    |> then(fn [a, b] -> [Enum.sort(a), Enum.sort(b)] end)
+  end
+
+  def part_01([first_list, second_list]) do
+    (0..(length(first_list) - 1))
+    |> Enum.reduce(0, fn index, acc -> 
+      acc + abs(Enum.at(first_list, index) - Enum.at(second_list, index))
+    end)
+  end
+
 end
