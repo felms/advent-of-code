6a25e9748e5c101fead4ab8a704e2ac4d9f09982
Felipe <felipemoreira1@gmail.com>
2024-12-02 16:33:58 -0300

    2024: Dia 02 - Problema 01

--- 2024/day02/lib/day02.ex
+++ 2024/day02/lib/day02.ex
@@ -1,18 +1,46 @@
 defmodule Day02 do
   @moduledoc """
-  Documentation for `Day02`.
+  Dia 02 do Advent of Code 2024
   """
 
-  @doc """
-  Hello world.
+  def run(mode \\ nil) do
+    input_file = if mode == :sample, do: "sample_input.txt", else: "input.txt"
 
-  ## Examples
+    input =
+      File.read!(input_file)
+      # Para evitar problemas no Windows
+      |> String.replace("\r", "")
+      |> parse_input()
 
-      iex> Day02.hello()
-      :world
+    {time, result} = :timer.tc(&part_01/1, [input])
 
-  """
-  def hello do
-    :world
+    IO.puts(
+      "\n==Part 01== \n\nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
+
+  end
+
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> line |> String.split() |> Enum.map(&String.to_integer/1) end)
+  end
+
+  def part_01(input) do
+    input
+    |> Enum.count(&(safe_decreasing?(&1) || safe_increasing?(&1)))
+  end
+
+  def safe_increasing?(report_line) do
+    report_line
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.all?(fn [a, b] -> b - a >= 1 && b - a <= 3 end)
+  end
+
+  def safe_decreasing?(report_line) do
+    report_line
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.all?(fn [a, b] -> a - b >= 1 && a - b <= 3 end)
   end
 end
