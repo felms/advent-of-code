dc544dd79d311ec70213978019d38126cb522855
felipe <felipemoreira1@gmail.com>
2024-12-07 20:03:05 -0300

    2024: Dia 06 - Problema 01

--- 2024/day06/lib/day06.ex
+++ 2024/day06/lib/day06.ex
@@ -1,18 +1,84 @@
 defmodule Day06 do
   @moduledoc """
-  Documentation for `Day06`.
+  Dia 06 do Advent of Code 2024
   """
 
-  @doc """
-  Hello world.
+  def run(mode \\ nil) do
+    input_file = if mode == :sample, do: "sample_input.txt", else: "input.txt"
 
-  ## Examples
+    input =
+      File.read!(input_file)
+      # Para evitar problemas no Windows
+      |> String.replace("\r", "")
+      |> parse_input
 
-      iex> Day06.hello()
-      :world
+    {time, result} = :timer.tc(&part_01/1, [input])
 
-  """
-  def hello do
-    :world
+    IO.puts(
+      "\n==Part 01== \n\nResult: #{result}" <>
+        "\nCalculated in #{time / 1_000_000} seconds\n"
+    )
   end
+
+  def part_01(input) do
+    pos = initial_pos(input)
+
+    exec_steps(pos, :up, input, MapSet.new([pos]), :ongoing)
+    |> MapSet.size()
+  end
+
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.with_index()
+    |> Enum.flat_map(&parse_line/1)
+    |> Map.new()
+  end
+
+  def parse_line({row, row_number}) do
+    row
+    |> String.graphemes()
+    |> Enum.with_index()
+    |> Enum.map(&{{row_number, elem(&1, 1)}, elem(&1, 0)})
+  end
+
+  def initial_pos(grid) do
+    grid
+    |> Map.to_list()
+    |> Enum.filter(fn {_point, v} -> v == "^" end)
+    |> hd
+    |> elem(0)
+  end
+
+  def exec_steps(_, _, _, visited_positions, :finished), do: visited_positions
+
+  def exec_steps(current_pos, direction, grid, visited_positions, :ongoing) do
+    {next_pos, next_dir} = find_next_position(current_pos, direction, grid)
+
+    if Map.get(grid, next_pos, :outside) == :outside do
+      exec_steps(current_pos, direction, grid, visited_positions, :finished)
+    else
+      exec_steps(next_pos, next_dir, grid, MapSet.put(visited_positions, next_pos), :ongoing)
+    end
+  end
+
+  def find_next_position(current_position, current_direction, grid) do
+    next_p = next_position(current_position, current_direction)
+
+    if Map.get(grid, next_p) == "#" do
+      find_next_position(current_position, turn_right(current_direction), grid)
+    else
+      {next_p, current_direction}
+    end
+  end
+
+  def next_position({r, c}, :up), do: {r - 1, c}
+  def next_position({r, c}, :left), do: {r, c - 1}
+  def next_position({r, c}, :down), do: {r + 1, c}
+  def next_position({r, c}, :right), do: {r, c + 1}
+
+  def turn_right(:up), do: :right
+  def turn_right(:right), do: :down
+  def turn_right(:down), do: :left
+  def turn_right(:left), do: :up
 end
